40be5a037ca1c3757b42cebf0629f8a90770689f
Josh Casto <josh@osx-jcasto.local>
2018-08-14 15:30:28 -0700

    completed up through exercise 12

---
+++ python/ex11.py
@@ -0,0 +1,27 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print("What is your favorite color?", end=' ')
+color = input()
+print("What is your profession?", end=' ')
+profession = input()
+print("What is your dream job?", end=' ')
+dream = input()
+
+print(f"So, when you started this exercise you talked about physical stats.\n")
+print(f"Now we're discussing more about you as a person. You've shared not \n")
+print(f"only what you do for a living {profession}, but what you wish you \n")
+print(f"wish you did for a living {dream}!\n\n")
+
+print(f"Is there some relationship between your {dream} and your favorite \n")
+print(f"color? Yes/No", end=' ')
+answer = input()
+
+print(f"\n So you believe the answer is {answer}!")
+
