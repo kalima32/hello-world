a3845d9546bf189249721bbc955d424a968474a8
Josh Casto <kalima32@yahoo.com>
2020-04-18 21:45:22 -0500

    updates with comments

--- python/crashcourse/number_tests.py
+++ python/crashcourse/number_tests.py
@@ -1,11 +1,15 @@
+# Setup this to test which version of python atom is running
 import sys
 print(sys.version)
+
+# This code is to test basic integers and how they're handled
 print(5+3)
 print(4*2)
 print(int(16/2))
 print(12-4)
 
-
+# This is work to test inserting a variable into a string and printing it to
+# standard out.
 fav_number = 72
 message = "One of my favorite numbers is: " + str(fav_number) + "!"
 print(message)
