93cc58cb6ac565979166283477d626ad537d0039
Josh Casto <kalima32@yahoo.com>
2020-04-18 21:17:14 -0500

    This is a quick script to verify python version and use a variable in a string!

--- python/crashcourse/number_tests.py
+++ python/crashcourse/number_tests.py
@@ -1,8 +1,11 @@
+import sys
+print(sys.version)
 print(5+3)
 print(4*2)
 print(int(16/2))
 print(12-4)
 
-fav_number = 72
 
-print(My favorite number is str(fav_number))
+fav_number = 72
+message = "One of my favorite numbers is: " + str(fav_number) + "!"
+print(message)
